ee3e22df0fbc0c35cea79e8b4ed417fdce931e56
Nairobi Bodden <breaniebodden@yahoo.com>
2022-02-22 20:53:06 -0600

    Se agregó el formulario del menu de agente y el boton de vehiculos al menu d eadministrador

---
+++ AutoRepuestos_Comestibles/MenuAgente.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoRepuestos_Comestibles
+{
+    /// <summary>
+    /// Interaction logic for MenuAgente.xaml
+    /// </summary>
+    public partial class MenuAgente : Window
+    {
+        public MenuAgente()
+        {
+            InitializeComponent();
+        }
+
+        private void TBShow(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 0.5;
+        }
+
+        private void TBHide(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 1;
+        }
+
+        private void PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            BtnShowHide.IsChecked = false;
+        }
+    }
+}
