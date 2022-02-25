ca6cbe467a86f2e6293afa73e62816d2f659e6c0
jpaolam <jp_martelg@unicah.edu>
2022-02-24 22:58:16 -0600

    Creacion Formulario proveedores

---
+++ AutoRepuestos_Comestibles/Vistas/Proveedores/Proveedores.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoRepuestos_Comestibles.Vistas.Proveedores
+{
+    /// <summary>
+    /// Lógica de interacción para Proveedores.xaml
+    /// </summary>
+    public partial class Proveedores : Page
+    {
+        public Proveedores()
+        {
+            InitializeComponent();
+        }
+    }
+}
