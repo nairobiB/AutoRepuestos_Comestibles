6341fe6f5cbb03c5aac3faf66a503a9618b66a3c
Emin Ramos Ramírez <eminramos16@gmail.com>
2022-02-28 23:03:59 -0600

    Conexion

---
+++ Capa_de_Datos/CD_Conexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Capa_de_Datos
+{
+    public class CD_Conexion
+    {
+        private readonly SqlConnection Conn = new SqlConnection("Data Source= RAMOS_C; initial catalog= BoddenRentals; integrated security=true");
+    }
+}
+
+
