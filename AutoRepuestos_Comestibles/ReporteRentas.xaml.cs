be65a4bdac20a1c6c4e7ba8947d3c3bfcc7aacd2
Nairobi Bodden <breaniebodden@yahoo.com>
2022-03-18 17:46:16 -0600

    Reporte de ventas

---
+++ AutoRepuestos_Comestibles/ReporteRentas.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoRepuestos_Comestibles
+{
+    /// <summary>
+    /// Interaction logic for ReporteRentas.xaml
+    /// </summary>
+    public partial class ReporteRentas : Window
+    {
+        public ReporteRentas()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(ReporteRentas_Loaded);
+        }
+
+        private void ReporteRentas_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.ReportViewer.ReportPath = System.IO.Path.Combine(Environment.CurrentDirectory, @"Resources\Busqueda_FacturaRenta.rdl");
+            this.ReportViewer.RefreshReport();           
+        }
+    }
+}
